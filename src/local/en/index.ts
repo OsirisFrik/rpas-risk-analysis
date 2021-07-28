0ee7f5d8866ad706e7efbdffe63e90491eedec38
osirisfrik <osiris@writecode.studio>
2021-07-27 23:16:56 -0700

    init form

--- src/local/en/index.ts
+++ src/local/en/index.ts
@@ -1,3 +1,4 @@
 export { default as login } from './login.json'
 export { default as btn } from './btn.json'
 export { default as error } from './error.json'
+export { default as analysis } from './analysis.json'
---
+++ src/views/Analysis.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-steps :active="step" direction="horizontal" process-status="wait" finish-status="finish">
+    <el-step
+      :title="$t('analysis.planification')"
+      :description="$t('analysis.descriptions.planification')"
+      icon="ri-flight-takeoff-line">
+      test
+    </el-step>
+  </el-steps>
+</template>
+<script>
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  name: 'AnalysisForm'
+})
+
+export default class AnalysisForm extends Vue {
+
+}
+</script>
