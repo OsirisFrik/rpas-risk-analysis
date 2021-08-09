cf725c0ade04e42f9602e2e2f7a2fd576d987d0e
osiris <osiris.lpz96@gmail.com>
2021-08-09 11:20:45 -0700

    WIP, create gitlab repo

---
+++ src/components/Forms/Analysis/Basic.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-row :gutter="24">
+    <el-col
+      :sm="12">
+      <el-form-item
+        :label="$t('analysis.form.pilotInCommand')"
+        size="normal">
+        <el-input
+          v-model="_value.pilot"
+          :placeholder="$t('analysis.form.pilotInCommand')"
+          size="normal" />
+      </el-form-item>
+    </el-col>
+  </el-row>
+</template>
+<script lang="ts">
+import { Options, prop, Vue } from 'vue-class-component'
+
+interface BasicData {
+  pilot: string
+  licence: string
+  estimeTime: string
+  start: string
+  end: string
+  airport: string
+  elevation: string
+  mark: string
+  dist: string
+}
+
+class BasicInfoAnalysisProps {
+  modelValue = prop<BasicData>({
+    type: Object,
+    required: true
+  })
+}
+
+@Options({
+  name: 'BasicInfoAnalysis'
+})
+
+export default class BasicInfoAnalysis extends Vue.with(BasicInfoAnalysisProps) {
+  get _value(): BasicData {
+    return this.modelValue
+  }
+
+  set _value(val: BasicData) {
+    this.$emit('update', val)
+  }
+}
+</script>
--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -4,18 +4,47 @@
       :title="$t('analysis.planification')"
       :description="$t('analysis.descriptions.planification')"
       icon="ri-flight-takeoff-line">
-      test
     </el-step>
   </el-steps>
+  <template>
+    <basic-info v-model="formData" />
+  </template>
 </template>
-<script>
+<script lang="ts">
 import { Options, Vue } from 'vue-class-component'
+import BasicInfo from '@/components/Forms/Analysis/Basic.vue'
 
 @Options({
-  name: 'AnalysisForm'
+  name: 'AnalysisForm',
+  components: {
+    BasicInfo
+  }
 })
 
 export default class AnalysisForm extends Vue {
+  public currentStape: number
+  public steps = {
+    basic: 0,
+    metar: 1,
+    observations: 2
+  }
 
+  public formData = {
+    pilot: '',
+    licence: '',
+    estimeTime: '',
+    start: '',
+    end: '',
+    airport: '',
+    elevation: '',
+    mark: '',
+    dist: ''
+  }
+
+  constructor() {
+    super()
+
+    this.currentStape = this.steps.basic
+  }
 }
 </script>
