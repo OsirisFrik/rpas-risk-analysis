bcc15c785b3666525164d96b1d9dbd1978188d08
Katherin <kserna@jelp.io>
2021-08-10 17:57:58 -0700

    Responsive style form analysis

--- src/components/Forms/Analysis/Basic.vue
+++ src/components/Forms/Analysis/Basic.vue
@@ -1,115 +1,158 @@
-<template>
-  <el-row :gutter="24">
-    <el-col
-      :sm="12">
-      <el-form-item
-        :label="$t('analysis.form.pilotInCommand')"
-        size="normal">
-        <el-input
-          v-model="_value.pilot"
-          :placeholder="$t('analysis.form.pilotInCommand')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
-    <el-col>
-      <el-form-item
-       :label="$t('analysis.form.license')">
-        <el-input
-          v-model="_value.estimedTime"
-          :placeholder="$t('analysis.form.license')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
-    <el-col>
-      <el-form-item
-       :label="$t('analysis.form.estimedTime')">
-        <el-input
-          v-model="_value.estimedTime"
-          :placeholder="$t('analysis.form.estimedTime')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
- <el-col>
-      <el-form-item
-       :label="$t('analysis.form.startHour')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.startHour')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
-
- <el-col>
-      <el-form-item
-       :label="$t('analysis.form.endHour')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.endHour')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
-  <el-col>
-      <el-form-item
-       :label="$t('analysis.form.lat')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.lat')"
-          size="normal" />
-      </el-form-item>
-    <el-form-item
-       :label="$t('analysis.form.long')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.long')"
-          size="normal" />
-      </el-form-item>
-   </el-col>
-<el-col>
-      <el-form-item
-      :label="$t('analysis.form.sitio')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.sitio')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
-<el-col>
-      <el-form-item
-       :label="$t('analysis.form.elevation')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.elevation')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
-
-    <el-col>
-      <el-form-item
-       :label="$t('analysis.form.criticalAirport')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.criticalAirport')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
-
-    <el-col>
-      <el-form-item
-       :label="$t('analysis.form.distance')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.distance')"
-          size="normal" />
-      </el-form-item>
-       <el-form-item
-       :label="$t('analysis.form.azimuth')">
-        <el-input
-          v-model="_value.start"
-          :placeholder="$t('analysis.form.azimuth')"
-          size="normal" />
-      </el-form-item>
-    </el-col>
-  </el-row>
+<template class="el-form">
+        <el-row :gutter="24">
+          <el-col :sm="12" :md="16" :lg="16" xl="16">
+            <el-form-item
+            :label="$t('analysis.form.pilotInCommand')">
+               <el-input
+                v-model="_value.start"
+                :placeholder="$t('analysis.form.pilotInCommand')"
+                size="normal">
+                <template #suffix>
+                      <i class="ri-user-fill"></i>
+                </template>
+             </el-input>
+            </el-form-item>
+        </el-col>
+          <el-col :sm="12" :md="8" :lg="8" xl="8">
+              <el-form-item
+              :label="$t('analysis.form.license')">
+                <el-input
+                    v-model="_value.start"
+                    :placeholder="$t('analysis.form.license')"
+                    size="normal">
+                        <template #suffix>
+                            <i class="ri-passport-fill"></i>
+                      </template>
+                  </el-input>
+               </el-form-item>
+           </el-col>
+       </el-row>
+         <el-row :gutter="24">
+          <el-col :sm="24" :md="24" :lg="8" xl="8">
+            <el-form-item
+            :label="$t('analysis.form.estimedTime')">
+              <el-input
+                v-model="_value.estimedTime"
+                :placeholder="$t('analysis.form.estimedTime')"
+                size="normal">
+                    <template #suffix>
+                          <i class="ri-timer-fill"></i>
+                      </template>
+                  </el-input>
+            </el-form-item>
+          </el-col>
+           <el-col :sm="24" :md="24" :lg="16" xl="16">
+             <el-form-item
+            :label="$t('analysis.form.startHour')">
+            <el-date-picker
+            class="date-picker"
+              v-model="start"
+              type="datetimerange"
+              start-placeholder="Start Date"
+              end-placeholder="End Date">
+            </el-date-picker>
+              </el-form-item>
+            </el-col>
+          </el-row>
+    <el-row :gutter="24">
+          <el-col class="lat-padding" :sm="24" :md="12" :lg="12" :xl="12">
+            <el-form-item
+            :label="$t('analysis.form.lat')">
+              <el-input
+                v-model="_value.start"
+                :placeholder="$t('analysis.form.lat')"
+                size="normal">
+                    <template #suffix>
+                        <i class="ri-china-railway-fill"></i>
+                      </template>
+                  </el-input>
+            </el-form-item>
+           </el-col>
+          <el-col class="long-padding" :sm="24" :md="12" :lg="12" :xl="12">
+            <el-form-item
+              :label="$t('analysis.form.long')">
+                <el-input
+                  v-model="_value.start"
+                  :placeholder="$t('analysis.form.long')"
+                  size="normal">
+                    <template #suffix>
+                        <i class="ri-china-railway-fill"></i>
+                      </template>
+                  </el-input>
+            </el-form-item>
+          </el-col>
+    </el-row>
+    <el-row :gutter="24">
+        <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+              <el-form-item
+              :label="$t('analysis.form.sitio')">
+                <el-input
+                  v-model="_value.start"
+                  :placeholder="$t('analysis.form.sitio')"
+                  size="normal">
+                   <template #suffix>
+                          <i class="ri-flight-land-fill"></i>
+                      </template>
+                  </el-input>
+              </el-form-item>
+            </el-col>
+        <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+              <el-form-item
+              :label="$t('analysis.form.elevation')">
+                <el-input
+                  v-model="_value.start"
+                  :placeholder="$t('analysis.form.elevation')"
+                  size="normal" >
+              <template #suffix>
+                         <i class="ri-flight-takeoff-fill"></i>
+                      </template>
+                  </el-input>
+              </el-form-item>
+            </el-col>
+      </el-row>
+    <el-row :gutter="24">
+        <el-col :xs="24">
+          <el-form-item
+          :label="$t('analysis.form.criticalAirport')">
+            <el-input
+              v-model="_value.start"
+              :placeholder="$t('analysis.form.criticalAirport')"
+              size="normal">
+                  <template #suffix>
+                        <i class="ri-map-pin-fill"></i>
+                      </template>
+                  </el-input>
+          </el-form-item>
+        </el-col>
+    </el-row>
+    <el-row :gutter="24">
+        <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+          <el-form-item
+          :label="$t('analysis.form.distance')">
+            <el-input
+              v-model="_value.start"
+              :placeholder="$t('analysis.form.distance')"
+              size="normal">
+               <template #suffix>
+                        <i class="ri-pin-distance-fill"></i>
+                      </template>
+                  </el-input>
+          </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+            <el-form-item
+            :label="$t('analysis.form.azimuth')">
+              <el-input
+                v-model="_value.start"
+                :placeholder="$t('analysis.form.azimuth')"
+                size="normal">
+                    <template #suffix>
+                        <i class="ri-line-chart-line"></i>
+                      </template>
+                  </el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
 </template>
 <script lang="ts">
 import { Options, prop, Vue } from 'vue-class-component'
--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -1,25 +1,32 @@
 <template>
-  <el-steps :active="currentStape" direction="horizontal" process-status="wait" finish-status="finish">
-    <el-step
-      :title="$t('analysis.planification')"
-      :description="$t('analysis.descriptions.planification')"
-      icon="ri-flight-takeoff-line">
-    </el-step>
-  </el-steps>
-  <el-form>
-    <template v-if="currentStape === steps.basic">
-      <basic-info v-model="formData" />
-    </template>
-    <template v-else-if="currentStape === steps.metar">
-      metar
-    </template>
-    <template v-else-if="currentStape === steps.observations">
-      observations
-    </template>
-    <span>
-      <button @click="currentStape += 1">next</button>
-    </span>
-  </el-form>
+  <div>
+    <el-steps :active="currentStape" direction="horizontal" process-status="wait" finish-status="finish">
+      <el-step
+        :title="$t('analysis.planification')"
+        :description="$t('analysis.descriptions.planification')"
+        icon="ri-flight-takeoff-line">
+      </el-step>
+    </el-steps>
+    <main class="el-main main-background ">
+    <div class="title-upper"> Inspecci&oacute;n de prevuelo para detectar amenazas en la operaci&oacute;n de rpas</div>
+  <el-container class="el-container-center input-analysis">
+    <el-form label-position="top">
+      <template v-if="currentStape === steps.basic">
+        <basic-info v-model="formData" />
+      </template>
+      <template v-else-if="currentStape === steps.metar">
+        metar
+      </template>
+      <template v-else-if="currentStape === steps.observations">
+        observations
+      </template>
+      <span>
+        <button @click="currentStape += 1">next</button>
+      </span>
+    </el-form>
+    </el-container>
+  </main>
+  </div>
 </template>
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
